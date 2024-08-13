c9e5875b1bcf27005966bc8665c2b9e49bd7f441
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-13 16:58:53 +0100

    Limit News Etc

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
 from send_email import send_email
 
+topic = "cars"
 api_key = "d7c9112300e84278a8748de393937687"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-13&sortBy=publishedAt&apiKey=d7c9112300e84278a8748de393937687"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2024-07-13&sortBy=publishedAt&apiKey={api_key}&language=en"
 
-body = ""
+body = "Subject: Today's News\n\n"
 request = requests.get(url)
 content = request.json()
-for article in content["articles"]:
-    body = body + str(article["title"]) + "\n" + str(article["description"]) + 2*"\n"
+
+for article in content["articles"][:20]:
+    body += str(article["title"]) + "\n" + str(article["description"]) + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(body)
