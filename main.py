844c2eb2556bd617b87365a6c560182e774fe409
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-13 16:43:12 +0100

    Send Email

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
+from send_email import send_email
 
 api_key = "d7c9112300e84278a8748de393937687"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-13&sortBy=publishedAt&apiKey=d7c9112300e84278a8748de393937687"
 
+body = ""
 request = requests.get(url)
 content = request.json()
 for article in content["articles"]:
-    title = article[title]
-    description = article["description"]
+    body = body + str(article["title"]) + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(body)
