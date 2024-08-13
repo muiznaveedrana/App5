58353f764000331a37319e9824cb62d8140a4520
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-13 17:07:37 +0100

    Basically Front-End!

---
+++ front-end.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import main
+st.title("Sign Up!")
+
+with st.form(key = "form"):
+    reciever = st.text_input("Your Email Address")
+    button = st.form_submit_button()
+    if button:
+        main.func(reciever)
+        st.info("Email Sent!")
--- send_email.py
+++ send_email.py
@@ -1,11 +1,9 @@
 import smtplib, ssl
-def send_email(message):
+def send_email(message, reciever):
     host = "smtp.gmail.com"
     port = 465
     username = "littlecoders10@gmail.com"
     password = "koxb ummm vlck ufgs"
-
-    reciever = "muiznaveedrana@gmail.com"
 
     my_context = ssl.create_default_context()
 
